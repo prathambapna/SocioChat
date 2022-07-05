1de8d8861355187f85d992db51f752ef515242ad
Pratham Bapna <manjubapna33@gmail.com>
2022-07-05 10:44:58 +0530

    starting with jwt

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,36 @@
-module.exports.index=function(req,res){
+const Post=require('../../../models/post');
+const Comment=require('../../../models/comment');
+
+module.exports.index=async function(req,res){
+    let posts=await Post.find({})
+    .sort('-createdAt')//sort acc to time of creation=>latest one showimg first
+    .populate('user')
+    .populate({
+        path:'comments',//as we have name the array in post schema as comments
+        populate:{
+            path:'user'//name the user in post schema as user
+        }
+    });
     return res.json(200,{
         message:"List of Posts",
-        posts:[]
+        posts:posts
     })
+}
+module.exports.destroy=async function(req,res){
+    try{
+        let post=await Post.findById(req.params.id);
+            post.remove();
+            await Comment.deleteMany({
+                post:req.params.id
+            });
+            return res.status(200).json({
+                message:"Post and associated comments deleted successfully"
+            });
+    }catch(err)
+    {
+        return res.status(500).json({
+            message:"Internal Server Error"
+        });  
+     }
+    
 }
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const cookieParser=require('cookie-parser');
 const app=express();
-const port=8100;
+const port=8000;
 const expressLayouts=require('express-ejs-layouts');
 const db=require('./config/mongoose');
 
