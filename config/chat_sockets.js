de67bfc326e51ffc0e250fd1017fcd52be6a1a4c
Pratham Bapna <manjubapna33@gmail.com>
2022-07-18 16:57:09 +0530

    added chat engine facility ,just have to solve cors part

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -8,5 +8,17 @@ module.exports.chatSockets=function(socketServer){
         socket.on('disconnect',function(){
             console.log('socket disconnected!');
         });
+
+        socket.on('join_room',function(data){
+            console.log('joining request received',data);
+            socket.join(data.chatroom);
+            io.in(data.chatroom).emit('user_joined',data);
+        })
+        
+        // CHANGE :: detect send_message and broadcast to everyone in the room
+        socket.on('send_message', function(data){
+            io.in(data.chatroom).emit('receive_message', data);
+        });
+
     });
 }
