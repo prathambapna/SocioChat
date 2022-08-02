c02770aba497f8874b5cbf1a78e7bd36e2d9e1fa
Pratham Bapna <manjubapna33@gmail.com>
2022-08-02 16:54:27 +0530

    gulp will be done later

--- config/view-helpers.js
+++ config/view-helpers.js
@@ -1,16 +1,16 @@
-const env=require('./environment');
-const fs=require('fs');
-const path=require('path');
+// const env=require('./environment');
+// const fs=require('fs');
+// const path=require('path');
 
-//this is to fetch the assets in production mode as we hv changed the files name by adding some hash string in public assets
+// //this is to fetch the assets in production mode as we hv changed the files name by adding some hash string in public assets
 
-module.exports=(app)=>{
-    app.locals.assetPath=function(filePath){
-        if(env.name=='development')
-        {
-            return filePath;
-        }
+// module.exports=(app)=>{
+//     app.locals.assetPath=function(filePath){
+//         if(env.name=='development')
+//         {
+//             return filePath;
+//         }
 
-        return '/' + JSON.parse(fs.readFileSync(path.join(__dirname,'../public/assets/rev-manifest.json')))[filePath];
-    }
-}
+//         return '/' + JSON.parse(fs.readFileSync(path.join(__dirname,'../public/assets/rev-manifest.json')))[filePath];
+//     }
+// }
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const logger=require('morgan');
 const cookieParser=require('cookie-parser');
 const app=express();
 
-require('./config/view-helpers')(app);
+// require('./config/view-helpers')(app);
 
 const port=8080;
 const expressLayouts=require('express-ejs-layouts');
