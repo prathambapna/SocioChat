478a886f6e6117d1c09d7437cb6f9dd3dd6e3c74
Pratham Bapna <manjubapna33@gmail.com>
2022-08-02 23:14:23 +0530

    project overview before gulp done

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -13,16 +13,6 @@ module.exports.chatSockets=function(socketServer){
         socket.on('disconnect', function(){
             console.log('socket disconnected!');
         });
-     
-    // let io=require('socket.io')(socketServer);
-
-    // io.sockets.on('connection',function(socket){
-    //     console.log('new connection received ',socket.id);
-
-    //     socket.on('disconnect',function(){
-    //         console.log('socket disconnected!');
-    //     });
-
         socket.on('join_room',function(data){
             console.log('joining request received',data);
             socket.join(data.chatroom);
